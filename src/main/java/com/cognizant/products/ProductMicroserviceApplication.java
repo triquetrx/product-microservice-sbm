ae002a12de69c30b69e9e42ce65ebcf087e01cd4
Khan <2146122@cognizant.com>
2022-09-04 15:11:08 +0530

    sbm-product-microservice

---
+++ src/main/java/com/cognizant/products/ProductMicroserviceApplication.java
@@ -0,0 +1,17 @@
+package com.cognizant.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
+public class ProductMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMicroserviceApplication.class, args);
+	}
+
+}
